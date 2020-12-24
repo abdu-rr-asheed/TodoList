939dbaaf63f61d749e438fdd6c6cc6d8dd1eedb1
abdu-rr-rasheed <abdurrasheed430@gmail.com>
2020-12-24 10:18:56 +0530

    merged

--- js/script.js
+++ js/script.js
@@ -1,60 +1,16 @@
-//jquary
-// $( function() {
-//   $(".box > .todoclass").sortable({
-//     connectWith: ".box > .doingclass",
-//     cursor: "move"
-//   });
-//   $(".box > .doingclass").sortable({
-//     connectWith: ".box > .doneclass",
-//     cursor: "move"
-//   });
-//   $(".box > .doneclass").sortable({
-//     connectWith: ".box > .todoclass",
-//     cursor: "move",
-//     zIndex: 9999
-//   });
-// });
-
-const draggables = document.querySelectorAll('.box')
-const containers = document.querySelectorAll('li')
-
-draggables.forEach(draggable => {
-  draggable.addEventListener('dragstart', () => {
-    draggable.classList.add('dragging')
-  })
-
-  draggable.addEventListener('dragend', () => {
-    draggable.classList.remove('dragging')
-  })
-})
-
-containers.forEach(container => {
-  container.addEventListener('dragover', e => {
-    e.preventDefault()
-    const afterElement = getDragAfterElement(container, e.clientY)
-    const draggable = document.querySelector('.dragging')
-    if (afterElement == null) {
-      container.appendChild(draggable)
-    } else {
-      container.insertBefore(draggable, afterElement)
-    }
-  })
-})
-
-function getDragAfterElement(container, y) {
-  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
-
-  return draggableElements.reduce((closest, child) => {
-    const box = child.getBoundingClientRect()
-    const offset = y - box.top - box.height / 2
-    if (offset < 0 && offset > closest.offset) {
-      return { offset: offset, element: child }
-    } else {
-      return closest
-    }
-  }, { offset: Number.NEGATIVE_INFINITY }).element
+function allowDrop(ev) {
+  ev.preventDefault();
 }
 
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
 
 // Add input
 let titleinput = document.querySelector(".Tdo");
@@ -83,7 +39,6 @@ let todoclass = document.querySelector(".todoclass")
 let doingclass = document.querySelector(".doingclass")
 let doneclass = document.querySelector(".doneclass")
 
-
 // ! Parent Array (Todo)
 const todoArr = [];
 
@@ -107,6 +62,8 @@ function refreshArr() {
     // li tag id
     newli.id = (i);
     newli.draggable = true;
+    newli.className = "dragls";
+    // newli.addEventListener("dragstart", drag);
     
     //Apend element
     newli.appendChild(newh5);
