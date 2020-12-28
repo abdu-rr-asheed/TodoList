24c7ba48992b7c5251056179fad1c46a53625c23
abdu-rr-rasheed <abdurrasheed430@gmail.com>
2020-12-28 09:59:03 +0530

    drdp done2

--- js/script.js
+++ js/script.js
@@ -83,55 +83,55 @@ function refreshArr() {
     //Edit Click icon Event 
     newedit.addEventListener('click', editIconbutton);
 
-    //!=================================================================
+    //! Drag and Drop =================================================================
 
     const draggables = document.querySelectorAll('.draggable')
-    const containers = document.querySelectorAll('.container')
+    const containers = document.querySelectorAll('.containerul')
 
-        draggables.forEach(draggable => {
-         draggable.addEventListener('dragstart', () => {
-          draggable.classList.add('dragging')
-  })
+    draggables.forEach(draggable => {
+      draggable.addEventListener('dragstart', () => {
+      draggable.classList.add('dragging')
+    })
 
-  draggable.addEventListener('dragend', () => {
-    // console.log(draggable.parentElement.id);
-    if (draggable.parentElement.id == 'doingid') {
-      todoArr[draggable.id].status = 'doingSt';
-    }else if (draggable.parentElement.id == 'doneEid') {
-      todoArr[draggable.id].status = 'doneSt';
-    }else if (draggable.parentElement.id == 'todoid') {
-      todoArr[draggable.id].status = 'todost';
+    draggable.addEventListener('dragend', () => {
+      // console.log(draggable.parentElement.id);
+      if (draggable.parentElement.id == 'doingid') {
+        todoArr[draggable.id].status = 'doingSt';
+      }else if (draggable.parentElement.id == 'doneEid') {
+        todoArr[draggable.id].status = 'doneSt';
+      }else if (draggable.parentElement.id == 'todoid') {
+        todoArr[draggable.id].status = 'todost';
+      }
+      draggable.classList.remove('dragging')
+      })
+    })
+
+    containers.forEach(container => {
+      container.addEventListener('dragover', e => {
+        e.preventDefault()
+        const afterElement = getDragAfterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if (afterElement == null) {
+          container.appendChild(draggable)
+        } else {
+          container.insertBefore(draggable, afterElement)
+        }
+      })
+    })
+
+    function getDragAfterElement(container, y) {
+      const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+      return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child }
+        } else {
+          return closest
+        }
+      }, { offset: Number.NEGATIVE_INFINITY }).element
     }
-    draggable.classList.remove('dragging')
-  })
-})
-
-containers.forEach(container => {
-  container.addEventListener('dragover', e => {
-    e.preventDefault()
-    const afterElement = getDragAfterElement(container, e.clientY)
-    const draggable = document.querySelector('.dragging')
-    if (afterElement == null) {
-      container.appendChild(draggable)
-    } else {
-      container.insertBefore(draggable, afterElement)
-    }
-  })
-})
-
-function getDragAfterElement(container, y) {
-  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
-
-  return draggableElements.reduce((closest, child) => {
-    const box = child.getBoundingClientRect()
-    const offset = y - box.top - box.height / 2
-    if (offset < 0 && offset > closest.offset) {
-      return { offset: offset, element: child }
-    } else {
-      return closest
-    }
-  }, { offset: Number.NEGATIVE_INFINITY }).element
-}
 
     //!=================================================================
     
